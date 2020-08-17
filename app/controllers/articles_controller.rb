be524a22b6c03bd38d1495406efad66b777d9871
Sumera Naaz <naazsumera63@gmail.com>
2020-08-17 12:13:26 -0700

    Add helper methods to application controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:show, :edit, :update, :destroy]
+
 	def show
 		
 	end
@@ -18,7 +19,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article=Article.new(article_params)
-		@article.user=User.first
+		@article.user=current_user
 		if @article.save
 			flash[:notice]="Article was saved successfully."
 			redirect_to @article
