30254220550afb653ff592d7395b68594a5eab6b
Sumera Naaz <naazsumera63@gmail.com>
2020-08-25 10:46:52 -0700

    Restrict actions at controller level for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:show, :update, :edit]
+	before_action :require_user, only: [:edit, :update]
+	before_action :require_same_user, only: [:edit, :update]
 
 	def new
 		@user=User.new
@@ -8,8 +10,8 @@ class UsersController < ApplicationController
 	def create
 		@user=User.new(user_params)
 		if @user.save
-			session[:user_id]=@user.id
-			flash[:notice]="Welcome to the Alpha Blog #{@user.username}. You have successfully signed up."
+			session[:user_id] = @user.id
+			flash[:notice] = "Welcome to the Alpha Blog #{@user.username}. You have successfully signed up."
 			redirect_to articles_path
 		else
 			render 'new'
@@ -23,7 +25,7 @@ class UsersController < ApplicationController
 	def update
 		
 		if @user.update(user_params)
-			flash[:notice]="Your account information was successfully updated"
+			flash[:notice] = "Your account information was successfully updated"
 			redirect_to @user
 		else
 			render 'edit'
@@ -46,5 +48,12 @@ class UsersController < ApplicationController
 
 	def set_user
 		@user=User.find(params[:id])
+	end
+
+	def require_same_user
+		if current_user != @user
+			flash[:notice] = "You can only edit your own account"
+			redirect_to @user
+		end
 	end
 end
